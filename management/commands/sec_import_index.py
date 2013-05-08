8ba382380f68b5985b64388df957bf7178d4c8b7
Jacob Fenton <jfenton@sunlightfoundation.com>
2013-05-07 22:48:26 -0400

    push some file paths to settings

--- management/commands/sec_import_index.py
+++ management/commands/sec_import_index.py
@@ -1,10 +1,12 @@
 from pysec.models import *
 from django.core.management.base import NoArgsCommand
-
+from django.conf import settings
 import urllib,os,re,os.path
 from zipfile import ZipFile
 import time
 
+
+DATA_DIR = settings.DATA_DIR
 def removeNonAscii(s): return "".join(i for i in s if ord(i)<128)
 
 # Gets the list of filings and download locations for the given year and quarter
@@ -15,7 +17,7 @@ def get_filing_list(year,qtr):
     print url
 
     # Download the data and save to a file
-    fn='data/company_%d_%d.zip' % (year,qtr)
+    fn='%s/company_%d_%d.zip' % (DATA_DIR, year,qtr)
 
     if not os.path.exists(fn):
         compressed_data=urllib.urlopen(url).read()
@@ -38,28 +40,28 @@ def get_filing_list(year,qtr):
                 'form':r[62:74].strip(),
                 'cik':r[74:86].strip(),
                 'date':date,
-				'quarter': quarter,
+                'quarter': quarter,
                 'filename':r[98:].strip()}
-				
+
         result.append(Index(**filing))
 
     return result
 
 
 class Command(NoArgsCommand):
-    help = "Download new files representing one month of 990s, ignoring months we already have"
+    help = "Download new files representing one month of 990s, ignoring months we already have. Each quarter contains hundreds of thousands of filings; will take a while to run. "
+    
     
     def handle_noargs(self, **options):
 
-        for year in range(2011,2014):
-            for qtr in range(1,5):        
-                if year<2013 or qtr<=1: 
-                    quarter = "%s%s" % (year,qtr)
-                    Index.objects.filter(quarter=quarter).delete()
-                    objs = get_filing_list(year,qtr)
-                    for obj in objs:
-                        try:
-                            obj.save()
-                        except:
-                            print 'error: %s' % obj
-                            pass
+        for year in range(2013,2014):
+            for qtr in range(1,2):        
+                quarter = "%s%s" % (year,qtr)
+                Index.objects.filter(quarter=quarter).delete()
+                objs = get_filing_list(year,qtr)
+                for obj in objs:
+                    try:
+                        obj.save()
+                    except:
+                        print 'error: %s' % obj
+                        pass
